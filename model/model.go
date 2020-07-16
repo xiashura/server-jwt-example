4a71d215723edfac82ee69fb929a3d08acfd2313
xiashura <plexanov@protonmail.com>
2020-07-16 06:15:09 +0500

    create model token

--- model/model.go
+++ model/model.go
@@ -1,14 +1,23 @@
 package model
 
-//User модель пользователя в приложение
+import "time"
+
+//User модель пользователя
 type User struct {
 	Email    string `json:"Email,omitempty"`
 	Name     string `json:"Name,omitempty"`
 	Password string `json:"Password,omitempty"`
 }
 
-//Client модель клиента в приложение
+//Client модель клиента
 type Client struct {
-	User  User   `json:"User,omitempty"`
-	Token string `json:"Token,omitempty"`
+	User  User  `json:"User,omitempty"`
+	Token Token `json:"Token,omitempty"`
+}
+
+//Token модель токена
+type Token struct {
+	Time       time.Time `json:"Time,omitempty"`
+	Authorized bool      `json:"Authorized,omitempty"`
+	Key        string    `json:"Key,omitempty"`
 }
