38c2597e07d17220dd8081d57bc52670368c7082
xiashura <plexanov@protonmail.com>
2020-09-02 20:34:24 +0500

    change middlware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -4,26 +4,53 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/xiashura/server-jwt-example/pkg/jwt"
 )
 
+//Registration jwt token
+func Registration(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var user jwt.Client
+
+		err := json.NewDecoder(r.Body).Decode(&user.User)
+		user.User.Time = time.Now()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		fmt.Println(user.User.Time.Unix())
+
+		token, err := user.Generate()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		user.Token.Key = token
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(user.Token)
+
+	}
+}
+
+//Authentication jwt token
 func Authentication(next http.HandlerFunc) http.HandlerFunc {
 
 	return func(w http.ResponseWriter, r *http.Request) {
 
 		var a jwt.Client
 
-		err := json.NewDecoder(r.Body).Decode(&a)
+		err := json.NewDecoder(r.Body).Decode(&a.Token)
 		if err != nil {
-
-			fmt.Println("work")
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
+
 		err = a.Valid()
+		fmt.Println(err)
 		if err != nil {
-			fmt.Println("work")
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
@@ -31,25 +58,4 @@ func Authentication(next http.HandlerFunc) http.HandlerFunc {
 		next(w, r)
 	}
 
-}
-
-func Mymiddleware(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var a jwt.Client
-
-		err := json.NewDecoder(r.Body).Decode(&a)
-
-		if err != nil {
-			fmt.Println("work")
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		token := jwt.Registration(a)
-
-		res, _ := json.Marshal(token)
-
-		w.Write(res)
-		next(w, r)
-	}
 }
